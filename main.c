a90485fa2cf33e8f05f154c4dfcaf60716787bf7
cyril <cyril.porez@laplateforme.io>
2024-02-10 16:57:12 +0100

    fix refactor code

--- main.c
+++ main.c
@@ -12,113 +12,110 @@ Node *readFileDirectory(char *filename, char *difficulty, char *category);
 
 char *maskWord(char *word)
 {
-    char *maskWord = malloc(strlen(word) + 1);
-    if (maskWord != NULL)
-    {
-        strcpy(maskWord, word);
-        int len = strlen(word);
+	char *maskWord = malloc(strlen(word) + 1);
+	if (maskWord != NULL)
+	{
+		strcpy(maskWord, word);
+		int len = strlen(word);
 
-        if (len > 2)
-        {
-            for (int i = 1; i < len - 1; i++)
-            {
-                maskWord[i] = '*';
-            }
-        }
-    }
-    return maskWord;
+		if (len > 2)
+		{
+			for (int i = 1; i < len - 1; i++)
+			{
+				maskWord[i] = '*';
+			}
+		}
+	}
+	return maskWord;
 }
 
 int main(int argc, char *argv[])
 {
-    char *dictionnary = argc > 2 ? argv[1] : "C:\\Users\\porez\\OneDrive\\Bureau\\hangman\\dictionnaire.txt";
-    char *difficulty = argc >= 3 ? argv[2] : NULL;
-    char *category = argc == 4 ? argv[3] : NULL;
+	char *dictionnary = argc > 2 ? argv[1] : "C:\\Users\\porez\\OneDrive\\Bureau\\hangman\\dictionnaire.txt";
+	char *difficulty = argc >= 3 ? argv[2] : NULL;
+	char *category = argc == 4 ? argv[3] : NULL;
 
-    rules();
+	rules();
 
-    bool test = true;
-    char yesNo;
-    while (test)
-    {
-        Node *listChain = readFileDirectory(dictionnary, difficulty, category);
+	bool test = true;
+	char yesNo;
+	while (test)
+	{
+		Node *listChain = readFileDirectory(dictionnary, difficulty, category);
 
-        char *findWord = getRandomElement(listChain);
-        char *maskedWord = maskWord(findWord);
+		char *findWord = getRandomElement(listChain);
+		char *maskedWord = maskWord(findWord), str[50];
+		bool victory = false;
+		int error = 0, len = 0, tryWord = 3, tryCharacter = 6, attemptNbr = 0;
 
-        char str[50];
+		while (error <= 9)
+		{
+			potence(maskedWord, error, tryWord, tryCharacter);
 
-        bool victory = false;
-        int error = 0, len = 0;
-        int tryWord = 3, tryCharacter = 6, attemptNbr = 0;
-        while (error <= 9)
-        {
-            potence(maskedWord, error, tryWord, tryCharacter);
+			if (error < 9)
+			{
+				printf("\nEntrer un mot ou un caractere : ");
+				fflush(stdout);
+				fgets(str, sizeof(str), stdin);
+				str[strcspn(str, "\n")] = 0;
+				len = strlen(str);
+				attemptNbr++;
+				printf("%d", attemptNbr);
+			}
 
-            if (error < 9)
-            {
-                printf("\nEntrer un mot ou un caractere : ");
-                fflush(stdout);
-                fgets(str, sizeof(str), stdin);
-                str[strcspn(str, "\n")] = 0;
-                len = strlen(str);
-                attemptNbr++;
-                printf("%d", attemptNbr);
-            }
+			if (len == 1)
+			{
+				int i = 0;
+				bool find = false;
+				while (findWord[i] != '\0')
+				{
+					if (findWord[i] == str[0])
+					{
+						find = true;
+						victory = true;
+						maskedWord[i] = str[0];
+					}
+					i++;
+				}
+				if (find == false)
+				{
+					tryCharacter--;
+					error++;
+				}
+			}
+			if (len > 1)
+			{
+				if (strcmp(str, findWord) == 0)
+				{
+					printf("YOU WIN !!!\n");
+					victory = true;
+				}
+				else
+				{
+					tryWord--;
+					error++;
+				}
+			}
+		}
+		if (victory == true)
+		{
+			char writeScore;
+			printf("Souhatez-vous inscrire votre score? (Y/N) ");
+			scanf("%c", &writeScore);
+			if (writeScore == 'Y')
+			{
+				char name[20];
+				printf("Veuiler entrer votre nom !");
+				scanf("%s", name);
+			}
+			printf("OK");
+		}
 
-            if (len == 1)
-            {
-                int i = 0;
-                bool find = false;
-                while (findWord[i] != '\0')
-                {
-                    if (findWord[i] == str[0])
-                    {
-                        find = true;
-                        victory = true;
-                        maskedWord[i] = str[0];
-                    }
-                    i++;
-                }
-                if (find == false)
-                {
-                    tryCharacter--;
-                    error++;
-                }
-            }
-            if (len > 1)
-            {
-                if (strcmp(str, findWord) == 0)
-                {
-                    printf("YOU WIN !!!\n");
-                    victory = true;
-                }
-                else
-                {
-                    tryWord--;
-                    error++;
-                }
-            }
-        }
-        if (victory == true)
-        {
-            char writeScore;
-            printf("Souhatez-vous inscrire votre score? (Y/N) ");
-            scanf("%c", &writeScore);
-            if (writeScore == 'Y')
-            {
-                char name[20];
-                printf("Veuiler entrer votre nom !");
-                scanf("%s", name);
-            }
-            printf("OK");
-        }
-
-        printf("Souhaitez-vous relancer une partie? (Y/N) ");
-        scanf(" %c", &yesNo);
-        while ((getchar()) != '\n')
-            ;
-        test = yesNo == 'N' ? false : true;
-    }
-    return 0;
+		printf("Souhaitez-vous relancer une partie? (Y/N) ");
+		scanf(" %c", &yesNo);
+		while ((getchar()) != '\n')
+			;
+		test = yesNo == 'N' ? false : true;
+	}
+	return 0;
 }
